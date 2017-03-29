95b5d4f8ef790ea4649775157809b1e34b8d2755
Brian Breaker <bbreaker@usgs.gov>
2017-03-29 14:57:57 -0500

    Update getRegInfo.R

--- R/getRegInfo.R
+++ R/getRegInfo.R
@@ -7,11 +7,12 @@ getRegInfo <- function(siteIDs) {
   require(dplyr, quietly = TRUE)
   
   # create a blank data frame that contains all of the information needed
-  regList <- data.frame(siteNo = as.character(), beginUnregDate = as.character(), 
-                        endUnregDate = as.character(), beginDatePk = as.character(), 
+  regList <- data.frame(siteNo = as.character(), beginRegDate = as.character(), 
+                        endRegDate = as.character(), beginDatePk = as.character(), 
                         endDatePk = as.character(), beginDtDv = as.character(), 
                         endDateDv = as.character(), lat = as.numeric(),
-                        long = as.numeric(), stringsAsFactors = F)
+                        long = as.numeric(), beginDtDvNew = as.character(),
+                        endDateDvNew = as.character(), stringsAsFactors = F)
   
   # create a dummy peak flow data frame
   pkDummy <- readNWISpeak("07048600", convertType = FALSE)
@@ -25,6 +26,10 @@ getRegInfo <- function(siteIDs) {
     
     # read in the peak flow file
     pkFile <- readNWISpeak(sitesX[i], convertType = FALSE)
+    
+    # change any -00 days to -01 days on the historic peaks in the file
+    pkFile$peak_dt <- dplyr::if_else(stringr::str_sub(pkFile$peak_dt, start = 9, end = 10) == "00", 
+                                     paste0(stringr::str_sub(pkFile$peak_dt, 1, 8), "01"), pkFile$peak_dt)
     
     # if there is no peak flow data, use the dummy
     if (nrow(pkFile) == 0) {
@@ -52,11 +57,11 @@ getRegInfo <- function(siteIDs) {
     
     # keep rows in the peak flow file that contain codes indicating 
     # regulation, channelization, or diversion
-    pkFileReg <- pkFile[grepl("5|6|C|1", pkFile$peak_cd),]
+    pkFileReg <- pkFile[grepl("5|6|C", pkFile$peak_cd),]
     
     # keep rows in the peak flow file that contain codes indicating 
     # regulation, channelization, or diversion
-    pkFileUnreg <- pkFile[!grepl("5|6|C|1", pkFile$peak_cd),]
+    pkFileUnreg <- pkFile[!grepl("5|6|C", pkFile$peak_cd),]
     
     # for sites that don't have any peak flows in the peak flow file
     if (nrow(pkTest) == 0) {
@@ -92,8 +97,8 @@ getRegInfo <- function(siteIDs) {
     else if (nrow(pkFileReg) > 1) {
       
       # add that information to the data frame
-      regListNew <- data.frame(siteNo = sitesX[i], beginUnregDate = as.character(pkFileReg[1,3]), 
-                               endUnregDate = as.character(pkFileReg[nrow(pkFileReg),3,]), 
+      regListNew <- data.frame(siteNo = sitesX[i], beginRegDate = as.character(pkFileReg[1,3]), 
+                               endRegDate = as.character(pkFileReg[nrow(pkFileReg),3,]), 
                                beginDatePk = as.character(pkFile[1,3]), 
                                endDatePk = as.character(pkFile[nrow(pkFile),3]),
                                beginDtDv = as.character(datFile[1,22]), 
@@ -127,13 +132,13 @@ getRegInfo <- function(siteIDs) {
   
   # create a new column of dates to start to think about a new start date
   # to use for this site if the peak flow file indicates 'altered' and 'unaltered' periods
-  regList$beginDtDvNew <- if_else(is.na(regList$beginUnregDate), regList$beginDtDvNew, 
-                                  firstDayNextMonth(regList$beginUnregDate))
+  regList$beginDtDvNew <- if_else(is.na(regList$beginRegDate), regList$beginDtDv, 
+                                  firstDayNextMonth(regList$beginDtDv))
   
   # create a new column of dates to start to think about a new end date
   # to use for this site if the peak flow file indicates 'altered' and 'unaltered' periods
-  regList$endDateDvNew <- if_else(is.na(regList$beginUnregDate), regList$endDateDv, 
-                                  lastDayPrevMonth(regList$endDateDv))
+  regList$endDateDvNew <- if_else(is.na(regList$beginRegDate), regList$endDateDv, 
+                                  lastDayPrevMonth(regList$beginRegDate))
   
   # return the final data frame
   return(regList)
