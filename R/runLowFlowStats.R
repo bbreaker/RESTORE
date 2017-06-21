0a68a6966fa142de0b90752048713cc48098a1ab
Brian Breaker <bbreaker@users.noreply.github.com>
2017-06-21 08:47:31 -0500

    Update runLowFlowStats.R

--- R/runLowFlowStats.R
+++ R/runLowFlowStats.R
@@ -33,7 +33,7 @@ runLowFlowStats <- function(site, startDate, endDate, yearType) {
                         oneQ10 = as.numeric(oneQten(dvs$Date, dvs$Flow, yearType)), 
                         thirtyQ5 = as.numeric(thirtyQfive(dvs$Date, dvs$Flow, yearType)), 
                         thirtyQ2 = as.numeric(thirtyQtwo(dvs$Date, dvs$Flow, yearType)), 
-                        harmMean = as.numeric(harmMean(dvs$Flow, na.rm = TRUE)),
+                        harMean = as.numeric(harmMean(dvs$Flow, na.rm = TRUE)),
                         comment = NA)
     
     return(newDF)
