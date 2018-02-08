ffd4274d2c12b5997256e1aedff96691e05df2cb
Brian Breaker <breakerb@gmail.com>
2018-02-08 13:06:04 -0600

    Update lastDayPrevMonth.R

--- R/lastDayPrevMonth.R
+++ R/lastDayPrevMonth.R
@@ -5,7 +5,7 @@ lastDayPrevMonth <- function(x) {
   dayx <- dplyr::if_else((lubridate::month(x) - 1) == 0, as.integer(31), 
                          as.integer(lubridate::days_in_month(lubridate::month(x) - 1)))
   monthx <- dplyr::if_else((lubridate::month(x) - 1) == 0, as.integer(12), 
-                           as.integer(month(x) - 1))
+                           as.integer(lubridate::month(x) - 1))
   yearx <- dplyr::if_else(lubridate::month(x) == 1, lubridate::year(x) - 1, 
                           as.double(lubridate::year(x)))
   x <- as.Date(paste0(yearx, "-", monthx, "-", dayx), 
